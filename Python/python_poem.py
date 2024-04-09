d0a2efee2be3fb562c2a7e45fe763c63159177d2
graphicalK <166463080+graphicalK@users.noreply.github.com>
2024-04-08 22:22:31 -0400

    Create python_poem.py

---
+++ Python/python_poem.py
@@ -0,0 +1,16 @@
+#Alexander Tran
+#1/29/2024
+#A short poem about Python for 1.03 Printing Strings
+
+def main():
+	print("_________________________________________")
+	print("				Python")
+	print("_________________________________________")
+	print("In the Netherlands, 1991,")
+	print("	Guido Van Rossum makes Python.")
+	print("	Coding text, games, drawing suns,")
+	print("	Used by everyone, like Google and icons.")
+	print("	And with the power of software and code,")
+	print("You can do anything, with Python!")
+	print("_________________________________________")
+main()
